ba6b60209d9cbfdbc9914251d2732e88878c0c0c
Jonathan Dunn <jonathandunn@Jonathans-MacBook-Air.local>
2024-02-02 10:11:56 -0500

    Updated the standalone app

--- client/server/app/patterns/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('auth', __name__)
-if 1 == 1:
-    from app.patterns import routes
--- client/server/app/web_frontend/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('webFrontend', __name__)
-
-if 1 == 1:
-    from app.web_frontend import routes
