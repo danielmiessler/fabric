f4f337d699700f287e350249363174bdb438f51b
jad2121 <jonathanascherdunn@gmail.com>
2024-02-11 11:52:20 -0500

    updated gui to include adding API key and updating patterns

--- client/gui/preload.js
+++ client/gui/preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
   invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
   on: (channel, func) => {
     ipcRenderer.on(channel, (event, ...args) => func(...args));
   },
