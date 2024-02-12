12f833b62cafaa4c5ef53201fdbd977879261ce4
Daniel Miessler <daniel@danielmiessler.com>
2024-02-11 23:31:08 -0800

    Add client dir.

---
+++ client/fabric/p.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import pyperclip
+
+pasted_text = pyperclip.paste()
+print(pasted_text)
---
+++ client/gui/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
+  on: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args));
+  },
+});
