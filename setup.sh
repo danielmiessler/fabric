f9df9db866d713e16a2fcc10567c578485e1c995
Scott Walsh <swalsh@coalitioninc.com>
2024-03-13 08:01:19 -0300

    WIP for pipx installations

--- setup.sh
+++ setup.sh
@@ -2,66 +2,68 @@
 
 # Check if pyproject.toml exists in the current directory
 if [ ! -f "pyproject.toml" ]; then
-  echo "Poetry could not find a pyproject.toml file in the current directory or its parents."
   echo "Please navigate to the project directory where pyproject.toml is located and rerun this script."
   exit 1
 fi
 
-# Installs poetry-based python dependencies
-echo "Installing python dependencies"
-poetry install
-
-# List of commands to check and add or update alias for
-# Add 'yt' and 'ts' to the list of commands
-commands=("fabric" "fabric-api" "fabric-webui" "ts" "yt")
+echo "Installing fabric via pipx"
+pipx install .
+pipx ensurepath
 
 # Path to the bootstrap file
-bootstrap_file="$HOME/.config/fabric/fabric-bootstrap.inc"
+fabric_path="$HOME/.config/fabric/"
+bootstrap="fabric-bootstrap.inc"
+context="context.md"
 
+bootstrap_file="$fabric_path$bootstrap"
+context_file="$fabric_path$context"
 # Ensure the directory for the bootstrap file exists
 mkdir -p "$(dirname "$bootstrap_file")"
 
-# Start the bootstrap file with a shebang if it doesn't already exist
-if [ ! -f "$bootstrap_file" ]; then
-  echo "#!/bin/bash" > "$bootstrap_file"
+if [ -e "$bootstrap_file" ]; then
+  echo "$bootstrap_file exists. Will only append new aliases"
+else
+  touch $bootstrap_file
+  echo "created $bootstrap_file"
+fi
+if [ -e "$context_file" ]; then
+  echo "$context_file exists. Doing nothing"
+else
+  touch $bootstrap_file
+  echo "created $bootstrap_file"
 fi
 
 # List of shell configuration files to update
 config_files=("$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.bash_profile")
 
-for config_file in "${config_files[@]}"; do
+for config_file in "${config_files[@]}"
+do
   # Check if the configuration file exists
-  if [ -f "$config_file" ]; then
+  if [ -e "$config_file" ]; then
     echo "Checking $config_file"
-
     # Ensure the bootstrap script is sourced from the shell configuration file
     source_line="if [ -f \"$bootstrap_file\" ]; then . \"$bootstrap_file\"; fi"
     if ! grep -qF -- "$source_line" "$config_file"; then
-      echo -e "\n# Load custom aliases\n$source_line" >> "$config_file"
+
+      echo -e "\n# Load custom aliases for fabric\n$source_line" >> "$config_file"
       echo "Added source command for $bootstrap_file in $config_file."
     fi
-    sed -i '' '/alias fabric=/d' "$config_file"
-    sed -i '' '/fabric --pattern/d' "$config_file"
+  fi
+done
 
-
+# set aliases for all known patterns
+echo "Checking alises:"
+for i in $(fabric --list)
+do
+  new="alias $i='fabric -cp $i'"
+  if ! grep -qF -- "$new" "$bootstrap_file"; then
+    echo $new >> $bootstrap_file
+    echo " - $i added to aliases"
   else
-    echo "$config_file does not exist."
+    echo " - $i alias exists- skipping"
   fi
+
 done
 
-# Add aliases to the bootstrap file
-for cmd in "${commands[@]}"; do
-  CMD_PATH=$(poetry run which $cmd 2>/dev/null)
-  if [ -z "$CMD_PATH" ]; then
-    echo "Command $cmd not found in the current Poetry environment."
-    continue
-  fi
-  
-  # Check if the alias already exists in the bootstrap file
-  if ! grep -qF "alias $cmd=" "$bootstrap_file"; then
-    echo "alias $cmd='$CMD_PATH'" >> "$bootstrap_file"
-    echo "Added alias for $cmd to $bootstrap_file."
-  fi
-done
-
-echo "Setup completed. Please restart your terminal or source your configuration files to apply changes."
+echo "Please restart your terminal or source $bootstrap_file with '. $bootstrap_file'."
+echo "Setup completed."
