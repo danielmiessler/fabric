8225bcfbada71dd2fb0894db95962e436f73f067
Fabian <fabian-wieland@gmx.de>
2024-06-04 20:48:09 +0200

    include error handeling and page selection for pdf extractor

--- installer/client/cli/pe.py
+++ installer/client/cli/pe.py
@@ -6,12 +6,27 @@ def main():
     parser = argparse.ArgumentParser(
         description='pdfExtractor extracts all the text from a pdf. By Fabian Wieland.')
     parser.add_argument('filename')
+    parser.add_argument("-p",'--page',  help='Select a specific page to extract')
     args = parser.parse_args()
-    doc = pymupdf.open(args.filename)  
+    if args.filename is None:
+        print("Error: No file provided.")
+        return
+    extract_content(args.filename, args.page)
+   
+
+
+def extract_content(filename,option):
+    doc = pymupdf.open(filename)  
     text=""
-    for page in doc:
-        text+=page.get_text()
-    print(text)
+    if option:
+        try:
+         print(doc.get_page_text(option))
+        except:
+         print("Error: {} is not a page in the provided document".format(option))
+    else:
+        for page in doc:
+            text+=page.get_text()
+        print(text)
 
 if __name__ == "__main__":
     main()
