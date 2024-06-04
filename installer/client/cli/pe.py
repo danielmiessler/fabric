4ac132edfc0f257985e98164318ee204f72e41f1
Fabian <fabian-wieland@gmx.de>
2024-06-04 22:18:19 +0200

    include pymupdf4llm for markdown of documents

--- installer/client/cli/pe.py
+++ installer/client/cli/pe.py
@@ -1,32 +1,44 @@
 import pymupdf
 import argparse
+import pymupdf4llm
+import pathlib
 
 def main():
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(
         description='pdfExtractor extracts all the text from a pdf. By Fabian Wieland.')
     parser.add_argument('filename')
-    parser.add_argument("-p",'--page',  help='Select a specific page to extract')
+    parser.add_argument("-p", "--page", help='Select a specific page to extract')
+    parser.add_argument("-m",'--markdown', action='store_true', help='Retrieve your document content in Markdown')
+    parser.add_argument("-s",'--save', action='store_true', help='Saves markdownfile (created with -m) utf-8 encoded as output.md')
     args = parser.parse_args()
     if args.filename is None:
         print("Error: No file provided.")
         return
-    extract_content(args.filename, args.page)
+    extract_content(args.filename, args)
    
 
 
-def extract_content(filename,option):
+def extract_content(filename,options):
     doc = pymupdf.open(filename)  
+    
     text=""
-    if option:
+    if options.page:
         try:
-         print(doc.get_page_text(option))
+         print(doc.get_page_text(options))
         except:
-         print("Error: {} is not a page in the provided document".format(option))
+         print("Error: {} is not a page in the provided document".format(options)) 
+    if (options.markdown):
+        md_text = pymupdf4llm.to_markdown(filename)
+        if(options.save):
+            # Write the text to some file in UTF8-encoding
+            pathlib.Path("output.md").write_bytes(md_text.encode())
+        print(md_text)
     else:
         for page in doc:
             text+=page.get_text()
-        print(text)
+        print(md_text)
+        
 
 if __name__ == "__main__":
     main()
