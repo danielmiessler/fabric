87322157fa63260478cbe69a6415d3e7bb972794
Andre <mail@andre-leifert.de>
2024-07-01 17:54:33 +0200

    fix: genai.list_models() precedes model names by models/ => just take the part after /

--- installer/client/cli/utils.py
+++ installer/client/cli/utils.py
@@ -446,7 +446,7 @@ class Standalone:
             genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
             for m in genai.list_models():
                 if 'generateContent' in m.supported_generation_methods:
-                    googleList.append(m.name)
+                    googleList.append(m.name.split("/")[-1])
         except:
             googleList = []
 
