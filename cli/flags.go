d3d15cc7a5e04117c5ae4e324291bbd39942e749
Eugen Eisler <eoeisler@gmail.com>
2024-09-15 20:38:19 +0200

    feat: use -r, --raw: Use defaults of model (don't send temperature etc.) and use the user role instead of the system role.

--- cli/flags.go
+++ cli/flags.go
@@ -23,7 +23,7 @@ type Flags struct {
 	TopP                    float64           `short:"T" long:"topp" description:"Set top P" default:"0.9"`
 	Stream                  bool              `short:"s" long:"stream" description:"Stream"`
 	PresencePenalty         float64           `short:"P" long:"presencepenalty" description:"Set presence penalty" default:"0.0"`
-	UserInsteadOfSystemRole bool              `short:"u" long:"user-instead-of-system" description:"Use the user role instead of the system role for the pattern"`
+	Raw                     bool              `short:"r" long:"raw" description:"Use the defaults of the model without sending chat options (like temperature etc.) and use the user role instead of the system role for patterns."`
 	FrequencyPenalty        float64           `short:"F" long:"frequencypenalty" description:"Set frequency penalty" default:"0.0"`
 	ListPatterns            bool              `short:"l" long:"listpatterns" description:"List all patterns"`
 	ListAllModels           bool              `short:"L" long:"listmodels" description:"List all available models"`
@@ -90,11 +90,11 @@ func readStdin() (string, error) {
 
 func (o *Flags) BuildChatOptions() (ret *common.ChatOptions) {
 	ret = &common.ChatOptions{
-		Temperature:             o.Temperature,
-		TopP:                    o.TopP,
-		PresencePenalty:         o.PresencePenalty,
-		FrequencyPenalty:        o.FrequencyPenalty,
-		UserInsteadOfSystemRole: o.UserInsteadOfSystemRole,
+		Temperature:      o.Temperature,
+		TopP:             o.TopP,
+		PresencePenalty:  o.PresencePenalty,
+		FrequencyPenalty: o.FrequencyPenalty,
+		Raw:              o.Raw,
 	}
 	return
 }
