ee368f417f91a684aa7c8d44531315594433e557
Eugen Eisler <eoeisler@gmail.com>
2024-09-29 21:35:20 +0200

    feat: add version updater bot

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/danielmiessler/fabric/cli"
 )
 
-// use to get latest tag, go install -ldflags "-X main.version=$(git describe --tags --always)" github.com/danielmiessler/fabric@latest
-var version = "dev" // Default version
-
 func main() {
 	_, err := cli.Cli(version)
 	if err != nil && !flags.WroteHelp(err) {
---
+++ version.go
@@ -0,0 +1,3 @@
+package main
+
+var version = "dev"
