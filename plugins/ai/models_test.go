bf5ec0042c87f00c98c4ed1df0857e3ec93517c3
Eugen Eisler <eugen.eisler@siemens.com>
2024-10-17 23:32:01 +0200

    feat: work on simplify config

--- plugins/ai/models_test.go
+++ plugins/ai/models_test.go
@@ -1,7 +1,6 @@
 package ai
 
 import (
-	"errors"
 	"testing"
 )
 
@@ -10,15 +9,15 @@ func TestNewVendorsModels(t *testing.T) {
 	if vendors == nil {
 		t.Fatalf("NewVendorsModels() returned nil")
 	}
-	if len(vendors.VendorsModels) != 0 {
+	if len(vendors.GroupsItems) != 0 {
 		t.Fatalf("NewVendorsModels() returned non-empty VendorsModels map")
 	}
 }
 
 func TestFindVendorsByModelFirst(t *testing.T) {
 	vendors := NewVendorsModels()
-	vendors.AddVendorModels("vendor1", []string{"model1", "model2"})
-	vendor := vendors.FindVendorsByModelFirst("model1")
+	vendors.AddGroupItems("vendor1", []string{"model1", "model2"}...)
+	vendor := vendors.FindGroupsByItemFirst("model1")
 	if vendor != "vendor1" {
 		t.Fatalf("FindVendorsByModelFirst() = %v, want %v", vendor, "vendor1")
 	}
@@ -26,27 +25,9 @@ func TestFindVendorsByModelFirst(t *testing.T) {
 
 func TestFindVendorsByModel(t *testing.T) {
 	vendors := NewVendorsModels()
-	vendors.AddVendorModels("vendor1", []string{"model1", "model2"})
-	foundVendors := vendors.FindVendorsByModel("model1")
+	vendors.AddGroupItems("vendor1", []string{"model1", "model2"}...)
+	foundVendors := vendors.FindGroupsByItem("model1")
 	if len(foundVendors) != 1 || foundVendors[0] != "vendor1" {
 		t.Fatalf("FindVendorsByModel() = %v, want %v", foundVendors, []string{"vendor1"})
-	}
-}
-
-func TestAddVendorModels(t *testing.T) {
-	vendors := NewVendorsModels()
-	vendors.AddVendorModels("vendor1", []string{"model1", "model2"})
-	models := vendors.GetVendorModels("vendor1")
-	if len(models) != 2 {
-		t.Fatalf("AddVendorModels() failed to add models")
-	}
-}
-
-func TestAddError(t *testing.T) {
-	vendors := NewVendorsModels()
-	err := errors.New("sample error")
-	vendors.AddError(err)
-	if len(vendors.Errs) != 1 {
-		t.Fatalf("AddError() failed to add error")
 	}
 }
