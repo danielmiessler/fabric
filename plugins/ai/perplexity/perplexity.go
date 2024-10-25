3b1ef24146b86ffd5277738fe45e66cd1c2ba8d7
Eugen Eisler <eoeisler@gmail.com>
2024-10-25 23:06:52 +0200

    Fix: remove set of the url; add todo to get models method

--- plugins/ai/perplexity/perplexity.go
+++ plugins/ai/perplexity/perplexity.go
@@ -7,7 +7,6 @@ import (
 func NewClient() (ret *Client) {
 	ret = &Client{}
 	ret.Client = openai.NewClientCompatible("Perplexity", "https://api.perplexity.ai", nil)
-	ret.Client.ApiBaseURL.Value = "https://api.perplexity.ai"
 	
 	return
 }
@@ -18,6 +17,7 @@ type Client struct {
 
 // The endpoint needed to list model from perplexity doesn't exist, so we 
 // will return a list of models that were available at the time of writing.
+// TODO use models config like it is done in Azure Vendor
 func (o *Client) ListModels() (ret []string, err error) {
 	ret = []string{"llama-3.1-sonar-small-128k-online", "llama-3.1-sonar-large-128k-online", "llama-3.1-sonar-huge-128k-online", "llama-3.1-sonar-small-128k-chat", "llama-3.1-sonar-large-128k-chat", "llama-3.1-8b-instruct", "llama-3.1-70b-instruct"}
 	return
