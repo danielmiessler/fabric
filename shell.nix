1ea3b4b3c58efc83a347265b04ce0ff29f5b005d
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2024-10-30 18:17:05 +0100

    modularize nix flake

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  gomod2nix,
+  goEnv,
+}:
+
+{
+  default = pkgs.mkShell {
+    nativeBuildInputs = [
+      pkgs.go
+      pkgs.gopls
+      pkgs.gotools
+      pkgs.go-tools
+      pkgs.goimports-reviser
+      gomod2nix
+      goEnv
+
+      (pkgs.writeShellScriptBin "update" ''
+        go get -u
+        go mod tidy
+        gomod2nix generate
+      '')
+    ];
+
+    shellHook = ''
+      echo -e "\033[0;32;4mHeper commands:\033[0m"
+      echo "'update' instead of 'go get -u && go mod tidy'"
+    '';
+  };
+}
