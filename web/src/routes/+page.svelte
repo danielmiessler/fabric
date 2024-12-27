84e3ff9386c945a4620e3eff793484803bab5010
John <john.connor.sec7@gmail.com>
2024-12-27 02:13:31 -0500

    Indented Main page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -22,20 +22,17 @@
   <h1 class="h1 text-8xl font-bold font-sans mt-8">
     <span class="bg-gradient-to-br to-blue-500 from-cyan-300 bg-clip-text text-transparent box-decoration-clone">fabric</span>
   </h1>
-
-  
-{#if augemented}
-<div class="py-2" transition:slide|local="{{delay: 250, duration: 3000, easing: quintOut }}">
-  <h2 class="h2 text-2xl text-center font-extrabold bg-gradient-to-br to-blue-500 from-cyan-300 bg-clip-text text-transparent pb-2">Human Flourishing via AI Augmentation</h2>
-  <div class="text-2xl">
-    <p class="mt-2 font-bold">Fill in the blanks... </p>
-    <i class="mt-2 font-medium justify-end">I believe one of the biggest problems in my world is <code class="code variant-filled-secondary">___________,</code> which is why I'm building/creating/doing <code class="code variant-filled-secondary">___________.</code>
+  {#if augemented}
+    <div class="py-2 mb-4" transition:slide|local="{{delay: 250, duration: 3000, easing: quintOut }}">
+      <h2 class="h2 text-2xl text-center font-extrabold bg-gradient-to-br to-blue-500 from-cyan-300 bg-clip-text text-transparent pb-2">Human Flourishing via AI Augmentation</h2>
+      <div class="text-2xl">
+        <p class="mt-2 font-bold">Fill in the blanks... </p>
+        <i class="mt-2 font-medium justify-end">I believe one of the biggest problems in my world is <code class="code variant-filled-secondary">___________,</code> which is why I'm building/creating/doing <code class="code variant-filled-secondary">___________.</code>
+      </div>
     </div>
-  </div>
   {/if}
 </div>
-  
-  
+
 {#if showTerminal}
   <div >
     <Terminal />
