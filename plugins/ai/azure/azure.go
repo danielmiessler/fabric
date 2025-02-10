e7c5359a0081fa5caef0aff403eca234bcae8399
TvisharajiK <tvisha.raji@keploy.io>
2025-02-11 02:53:36 +0530

    Feat: Increase unit test coverage from 0 to 100% in the AI module using Keploy's Agent

--- plugins/ai/azure/azure.go
+++ plugins/ai/azure/azure.go
@@ -1,38 +1,35 @@
 package azure
 
 import (
-	"github.com/danielmiessler/fabric/plugins"
-	"github.com/danielmiessler/fabric/plugins/ai/openai"
-	"strings"
-
-	goopenai "github.com/sashabaranov/go-openai"
+    "github.com/danielmiessler/fabric/plugins"
+    "github.com/danielmiessler/fabric/plugins/ai/openai"
+    "strings"
+    goopenai "github.com/sashabaranov/go-openai"
 )
 
 func NewClient() (ret *Client) {
-	ret = &Client{}
-	ret.Client = openai.NewClientCompatible("Azure", "", ret.configure)
-	ret.ApiDeployments = ret.AddSetupQuestionCustom("deployments", true,
-		"Enter your Azure deployments (comma separated)")
+    ret = &Client{}
+    ret.Client = openai.NewClientCompatible("Azure", "", ret.configure)
+    ret.ApiDeployments = ret.AddSetupQuestionCustom("deployments", true,
+        "Enter your Azure deployments (comma separated)")
 
-	return
+    return
 }
 
 type Client struct {
-	*openai.Client
-	ApiDeployments *plugins.SetupQuestion
+    *openai.Client
+    ApiDeployments *plugins.SetupQuestion
 
-	apiDeployments []string
+    apiDeployments []string
 }
 
 func (oi *Client) configure() (err error) {
-	oi.apiDeployments = strings.Split(oi.ApiDeployments.Value, ",")
-	oi.ApiClient = goopenai.NewClientWithConfig(goopenai.DefaultAzureConfig(oi.ApiKey.Value, oi.ApiBaseURL.Value))
-	return
+    oi.apiDeployments = strings.Split(oi.ApiDeployments.Value, ",")
+    oi.ApiClient = goopenai.NewClientWithConfig(goopenai.DefaultAzureConfig(oi.ApiKey.Value, oi.ApiBaseURL.Value))
+    return
 }
 
 func (oi *Client) ListModels() (ret []string, err error) {
-	// Azure Open Client has models and deployments. We need to get the deployments to use them for chat
-	// There is no easy way to get the deployments from the API, so we need to ask the user to provide them
-	ret = oi.apiDeployments
-	return
+    ret = oi.apiDeployments
+    return
 }
--- plugins/ai/groq/groq.go
+++ plugins/ai/groq/groq.go
@@ -1,15 +1,17 @@
 package groq
 
 import (
-	"github.com/danielmiessler/fabric/plugins/ai/openai"
+    "github.com/danielmiessler/fabric/plugins/ai/openai"
 )
 
+// NewClient initializes and returns a new Groq Client.
 func NewClient() (ret *Client) {
-	ret = &Client{}
-	ret.Client = openai.NewClientCompatible("Groq", "https://api.groq.com/openai/v1", nil)
-	return
+    ret = &Client{}
+    ret.Client = openai.NewClientCompatible("Groq", "https://api.groq.com/openai/v1", nil)
+    return
 }
 
+// Client wraps the openai.Client to provide additional functionality specific to Groq.
 type Client struct {
-	*openai.Client
+    *openai.Client
 }
