3ab923100bf4628e67a38c666680221cbb5bd7ae
Matt Joyce <matt.joyce@gmail.com>
2025-04-19 07:01:01 +1000

    Revert "Remove auto‑generated version files from PR"
    This reverts commit b9c4ff845640d50242cc3cf6abc80dfb10be27ed.

--- nix/pkgs/fabric/version.nix
+++ nix/pkgs/fabric/version.nix
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 "1.4.173"
+||||||| b9c4ff8 (Remove auto‑generated version files from PR)
+=======
+"..1"
+>>>>>>> parent of b9c4ff8 (Remove auto‑generated version files from PR)
--- version.go
+++ version.go
@@ -1,3 +1,8 @@
 package main
 
+<<<<<<< HEAD
 var version = "v1.4.173"
+||||||| b9c4ff8 (Remove auto‑generated version files from PR)
+=======
+var version = "v..1"
+>>>>>>> parent of b9c4ff8 (Remove auto‑generated version files from PR)
