a2e618e11c5bacca975c15e3d6ebc3ad1090296e
Kayvan Sylvan <kayvan@sylvan.com>
2025-07-15 22:02:16 -0700

    perf: add regex caching to `StripThinkBlocks` function for improved performance

--- internal/domain/think.go
+++ internal/domain/think.go
@@ -1,15 +1,32 @@
 package domain
 
-import "regexp"
+import (
+	"regexp"
+	"sync"
+)
 
 // StripThinkBlocks removes any content between the provided start and end tags
 // from the input string. Whitespace following the end tag is also removed so
 // output resumes at the next non-empty line.
+var (
+	regexCache = make(map[string]*regexp.Regexp)
+	cacheMutex sync.Mutex
+)
+
 func StripThinkBlocks(input, startTag, endTag string) string {
 	if startTag == "" || endTag == "" {
 		return input
 	}
-	pattern := "(?s)" + regexp.QuoteMeta(startTag) + ".*?" + regexp.QuoteMeta(endTag) + "\\s*"
-	re := regexp.MustCompile(pattern)
+
+	cacheKey := startTag + "|" + endTag
+	cacheMutex.Lock()
+	re, exists := regexCache[cacheKey]
+	if !exists {
+		pattern := "(?s)" + regexp.QuoteMeta(startTag) + ".*?" + regexp.QuoteMeta(endTag) + "\\s*"
+		re = regexp.MustCompile(pattern)
+		regexCache[cacheKey] = re
+	}
+	cacheMutex.Unlock()
+
 	return re.ReplaceAllString(input, "")
 }
